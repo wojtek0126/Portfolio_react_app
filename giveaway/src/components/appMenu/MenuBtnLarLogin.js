4fe962548bba9cf14a258dc5aec9e09d56c5c5e3
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-05-09 21:49:02 +0200

    about us section done rwd

--- giveaway/src/components/appMenu/MenuBtnLarLogin.js
+++ giveaway/src/components/appMenu/MenuBtnLarLogin.js
@@ -3,7 +3,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 const MenuBtnLarLogin = ({text, id, linkTo}) => {
     return (
-        <Link to={linkTo} className="btn menu-bottom-button" id={id}>{text}</Link>
+        <Link to={linkTo} className="btn menu-bottom-button menu-btn-login" id={id}>{text}</Link>
     )
 }
 
